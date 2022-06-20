f516201fb2bf8afea3972b7c58e8699bb8e680a4
artemevskih <artemevskih@gmail.com>
2022-06-20 13:08:58 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
+/* eslint-disable no-console */
 const getRandomNuber = (min, max) => {
   if (min < 0 || max < 0) {
     return console.log('Некорректные значения');
   } else {
-    let minNumber = (min < max) ? min : max;
-    let maxNumber = (max > min) ? max : min;
-
-    return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber + (1 * Math.random()));
+    const minNumber = (min < max) ? min : max;
+    const maxNumber = (max > min) ? max : min;
+    return Math.floor(Math.random() * (maxNumber - minNumber + (1 * Math.random())) + minNumber);
   }
-}
+};
 
 const checkStringLength = (str, maxLength) => {
   if (typeof str === 'string' || typeof maxLegth === 'number') {
-    let result = (str.length > maxLength) ? false : true;
-    return result;
+    return !(str.length > maxLength);
   } else {
     return console.log('Некорректные значения');
   }
-}
+};
 
 console.log(checkStringLength('asdfaskdfjlasdfkj', 2));
 console.log(checkStringLength('asdfaskdfjlasdfkj', 20));
@@ -24,4 +23,5 @@ console.log(checkStringLength(534523, true));
 
 console.log(getRandomNuber(1, 10));
 console.log(getRandomNuber(10, 1));
+console.log(getRandomNuber(5, 5));
 console.log(getRandomNuber(-10, 1));
